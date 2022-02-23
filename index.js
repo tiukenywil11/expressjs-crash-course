eb26c5c644bca017af06e8ed962758b4c01c4dcc
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-23 11:04:43 +0800

    Added the following files to render multiple static html pages: index.js, index.html, about.html, style.css:

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 // path is a dependency of node, which gets device's path
 const path = require('path');
 
-// Create an endpoint for the main page
+/*
+-- This was commented out, and replaced by static directory
+-- Create an endpoint for the main page
 app.get('/', (req, res) => {
-    // res.send('<h1> Hello World </h1>');
+    -- res.send('<h1> Hello World </h1>');
 
-    // Using sendFile uses an entire html file, instead of code snippets from
+    -- Using sendFile uses an entire html file, instead of code snippets from
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+*/
+
+// Set a static folder, set 'public' as default directory used
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Get port from environment variable, or on port 5000
 const PORT = process.env.PORT || 5000;
