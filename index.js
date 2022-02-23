3801e3aed5456dd0a7d3b4cd2a760c05a96430f6
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-23 10:36:10 +0800

    Added the following files to render html pages: index.js, index.html:

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+// path is a dependency of node, which gets device's path
+const path = require('path');
 
 // Create an endpoint for the main page
 app.get('/', (req, res) => {
-    res.send('<h1> Hello World </h1>');
+    // res.send('<h1> Hello World </h1>');
+
+    // Using sendFile uses an entire html file, instead of code snippets from
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Get port from environment variable, or on port 5000
