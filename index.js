9d01d4dfe3a03c6531581aef17734afd14838a54
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-24 14:04:28 +0800

    Appended the following files to add error handling on api to get single element: index.js

--- index.js
+++ index.js
@@ -34,10 +34,21 @@ app.get('/api/members/:id', (req, res) => {
     // Sends a request to get values based in id (req.params = returns for a string format)
     // res.send(req.params.id);
 
-    // Parses members from javascript object to json format
-    res.json(members.filter(member => 
-        member.id === parseInt(req.params.id)
-    ));
+    // The higher order array some returns true or false based on te condition provided 
+    const found = members.some(member => 
+        member.id === parseInt(req.params.id) 
+    )
+
+    if(found) {
+        // Parses members from javascript object to json format
+        res.json(members.filter(member => 
+            member.id === parseInt(req.params.id)
+        ));
+    } else {
+        res.status(400).json(
+            { msg: `No member with th id of ${req.params.id}`}
+        )
+    }
 })
 
 // Set a static folder, set 'public' as default directory used
